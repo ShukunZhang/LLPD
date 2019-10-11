6548903bcef4c9b165d54e6eda09dd3e83706ed3
Shukun_Zhang <34422187+ShukunZhang@users.noreply.github.com>
2019-10-11 03:49:57 -0400

    Update paviaU.m

--- LLPD/HyperspectralImages/paviaU.m
+++ LLPD/HyperspectralImages/paviaU.m
@@ -1,25 +1,61 @@
-clear all;
 
-addpath(genpath('../../../../LLPD_Code'));
+clear all;
+close all;
+addpath(genpath('../../LLPD'));
+addpath(genpath('/Users/shukunzhang/Desktop/Research/annavlittle-llpd_code-5d8ee720b9f2/LLPD_SpectralClustering/'));
+
+
 
 load('paviaU_gt.mat');
 LabelsGT=paviaU_gt;
 
 X=load('paviaU.mat');
 X=X.paviaU;
+figure; imagesc(LabelsGT);
+figure; imagesc(sum(X,3));
+%%
+Y=X([300:340],[140:189],:);
+L=LabelsGT([300:340],[140:189]);
+%figure;imagesc(LabelsGT);
+figure; imagesc(sum(Y,3));
 
-X=reshape(X,610*340,103);
-
+X=reshape(Y,41*50,103);
+LabelsGT=double(L);
+%%
 SetDefaultParameters
+LLPDopts.KNN = 600 ; %Number of nearest neighbors in underlying graph
+LLPDopts.UseFixedNumScales = 0; %Forces a fixed number of scales in LLPD approximation
+LLPDopts.LogRatio=1.1;
 
-DenoisingOpts.Method='Cutoff';
-DenoisingOpts.Cutoff = 4.5;
-
+%DenoisingOpts.KNN = 100;
+DenoisingOpts.Method='ExamineFigure'; %Look at figure to denoise, or set a cutoff?
+DenoisingOpts.Cutoff = 1000;
 SpectralOpts.SigmaScaling = 'Manual';
-SpectralOpts.SigmaValues = linspace(1.1, 3.5, 20);
+SpectralOpts.SigmaValues = linspace(100, 700, 10);
+SpectralOpts.NumEig = 10;
+ComparisonOpts.RunEucSC = 0;
+ComparisonOpts.RunKmeans = 0;
+ComparisonOpts.EucSCSigmaScaling = 'Automatic'; 
+ComparisonOpts.EucSCSigmaValues = linspace(1, 5, 5);
 
-ComparisonOpts.RunEucSC = 1;
-ComparisonOpts.EucSCSigmaScaling = 'Manual'; 
-ComparisonOpts.EucSCSigmaValues = linspace(3.3, 6, 20);
+SpatialReg.UseReg = 1;
+SpatialReg.Width = 41;
+SpatialReg.Height = 50;
+SpatialReg.Sigma = 100;
+SpatialReg.R=1;
+SpatialReg.r=15;
 
-GeneralScript_LLPD_SC
+MajorV.Use = 1;
+MajorV.Radius = 3;
+MajorV.VoteRate = 0.7;
+MajorV.Width = 41;
+MajorV.Height = 50;
+
+GeneralScript_LLPD_SC;
+
+
+%%
+figure;imagesc(reshape(LabelsGT,41,50));
+figure;imagesc(reshape(Labels_LLPD_SC_FullData_NS,41,50));
+figure;imagesc(reshape(Labels_LLPD_SC_FullData,41,50));
+figure;imagesc(reshape(Labels_LLPD_SC_FullData,41,50).*LabelsGT);
