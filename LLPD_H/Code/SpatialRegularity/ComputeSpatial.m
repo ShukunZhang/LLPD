9b7353ddb08b73fa8f7920c1c61cc0fc0c4d9988
Shukun_Zhang <34422187+ShukunZhang@users.noreply.github.com>
2019-11-01 17:10:29 -0400

    Fix Compute Spatial Matrix Bugs

--- LLPD_H/Code/SpatialRegularity/ComputeSpatial.m
+++ LLPD_H/Code/SpatialRegularity/ComputeSpatial.m
@@ -1,37 +1,25 @@
 function LLPD_S = ComputeSpatial(SpatialReg)
 w=SpatialReg.Width;
 h=SpatialReg.Height;
-sigma=SpatialReg.Sigma;
+r=SpatialReg.r;
+sz=[w h];
+LLPD_S=zeros(w*h,w*h);
 
-    LLPD_S=zeros(w*h,w*h);
-    
-    if SpatialReg.R == 1
-        r=SpatialReg.r;
 
-        for e=1:w*h %for each data point
-            q=floor(e/w); 
-            R=[max(mod(e,w)-r,1):min(mod(e,w)+r,w)];
-            for i=0:r
-            % add the index of points with radius = i 
-                idx_l=max((q-i),0)*w +R;
-                idx_r=min((q+i),h)*w+R;
-                LLPD_S(e,idx_l)=1;
-                LLPD_S(e,idx_r)=1;
-        
-            end
-        end   
-    else
-        for m = 1:w*h
-            i1 = floor((m-1)/h);
-            j1= mod(m-1,w);
-            for n=1:w*h
-                i2 = floor((n-1)/h);
-                j2= mod(n-1,w);
-                LLPD_S(m,n) = exp(-((i1-i2)^2 + (j1-j2)^2)/sigma.^2);
-            end
+for e=1:w*h
+    [e_row,e_col]=ind2sub(sz,e);
+    r_left=max(1,e_row-r);
+    r_right=min(w,e_row+r);
+    c_left=max(1,e_col-r);
+    c_right=min(h,e_col+r);
+    for i=r_left:r_right
+        for j=c_left:c_right
+            r_c=sub2ind(sz,i,j);
+            LLPD_S(e,r_c)=1;
         end
+    end     
+end
+   
     
-    end
-
 end
 
